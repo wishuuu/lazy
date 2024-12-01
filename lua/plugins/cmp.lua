47825a8339d4f21b79f98fbaabc291a975b3f522
Oskar Wiszowaty <ppwiszu@gmail.com>
2024-12-01 16:19:22 +0100

    Disable enter as cmp confirm

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -9,6 +9,7 @@ return {
       ["<C-j>"] = cmp.mapping.select_next_item(),
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-l>"] = cmp.mapping.confirm({ select = true }),
+      ["<CR>"] = cmp.config.disable,
     })
   end,
 }
